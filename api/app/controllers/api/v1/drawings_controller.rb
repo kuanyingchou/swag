2ff60649ef1f56a3434b7062cb0b9b807cbafcff
Devin <devinmui@yahoo.com>
2015-09-19 14:49:38 -0700

    added tables and controllers to rails app

---
+++ api/app/controllers/api/v1/drawings_controller.rb
@@ -0,0 +1,6 @@
+module Api
+	module V1
+		class DrawingsController < ApplicationController
+		end
+	end
+end
---
+++ api/db/schema.rb
@@ -0,0 +1,62 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150919214216) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "user_id"
+    t.integer  "drawing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["drawing_id"], name: "index_comments_on_drawing_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "drawings", force: :cascade do |t|
+    t.string   "path"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "drawings", ["user_id"], name: "index_drawings_on_user_id", using: :btree
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "drawing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["drawing_id"], name: "index_likes_on_drawing_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_foreign_key "comments", "drawings"
+  add_foreign_key "comments", "users"
+  add_foreign_key "drawings", "users"
+  add_foreign_key "likes", "drawings"
+  add_foreign_key "likes", "users"
+end
